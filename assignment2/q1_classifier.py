7e8c6ec71a639923e151c89a00c265ba54a4c66d
Oleksandr Maksymenko <alexandr.maximenko@gmail.com>
2018-10-12 11:21:22 +0300

    add placeholders to SoftmaxModel

--- assignment2/q1_classifier.py
+++ assignment2/q1_classifier.py
@@ -46,6 +46,8 @@ class SoftmaxModel(Model):
             self.labels_placeholder
         """
         ### YOUR CODE HERE
+        self.input_placeholder = tf.placeholder(tf.float32, shape=(self.config.batch_size, self.config.n_features))
+        self.label_placeholder = tf.placeholder(tf.int32, shape=(self.config.batch_size, self.config.n_classes))
         ### END YOUR CODE
 
     def create_feed_dict(self, inputs_batch, labels_batch=None):
@@ -69,6 +71,10 @@ class SoftmaxModel(Model):
             feed_dict: The feed dictionary mapping from placeholders to values.
         """
         ### YOUR CODE HERE
+        feed_dict = {
+            self.input_placeholder: inputs_batch,
+            self.label_placeholder: labels_batch,
+        }
         ### END YOUR CODE
         return feed_dict
 
